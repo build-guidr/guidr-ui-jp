67b760c6d24cd8b36003418b277471b984fa5643
EoPlex <you@example.com>
2019-04-18 08:41:31 -0700

    most updated version

---
+++ js/index.js
@@ -0,0 +1,20 @@
+
+//toggle menu feature
+
+
+const toggleMenu = function() {
+  const menu = document.querySelector(".menu");
+  menu.classList.toggle("menu--open");
+}
+  
+  const menuButton = document.querySelector(".menu-button");
+  
+
+  //Event Listeners
+
+  menuButton.addEventListener("click", function() {
+   toggleMenu();
+  }); 
+
+
+  
