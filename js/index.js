7602c892375baa841cf3af3b0dbb3d980f335f7e
EoPlex <you@example.com>
2019-04-18 21:35:04 -0700

    js nav dom

--- js/index.js
+++ js/index.js
@@ -6,14 +6,18 @@ const toggleMenu = function() {
   menu.classList.toggle("menu--open");
 }
   
-  const menuButton = document.querySelector(".menu-button");
+const menuButton = document.querySelector(".menu-button");
   
-
-  //Event Listeners
-
   menuButton.addEventListener("click", function() {
    toggleMenu();
   }); 
 
 
-  
+  let nav = document.querySelector('nav');
+  let navItems = nav.querySelectorAll('a');
+
+  let newNavItem1 = document.createElement("a");
+    newNavItem1.textContent = "Contact";
+    newNavItem1.style.color = "white";
+    newNavItem1.setAttribute("href", "contact.html"); 
+    nav.appendChild(newNavItem1);
